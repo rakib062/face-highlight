8764ac71360bcec35c10c2b8ecf3bf563dfdebad
rakib062 <rakib_cse_062@yahoo.com>
2021-11-13 16:12:36 +0100

    face detection and git creation with face boxes

--- create_gif.py
+++ create_gif.py
@@ -9,15 +9,16 @@ import matplotlib.pyplot as plt
 import glob
 import sys, os, subprocess
 from pathlib import Path
-
+import csv
 
 
 indir = sys.argv[1]
 outdir = sys.argv[2]
+box_dict_path = sys.argv[3]
 
-def create_black_images(img):
+def create_black_images(img, face_center, radius, outdir="temp/"):
     mask = np.full(img.shape, 1, dtype=np.uint8)
-    mask = cv2.circle(mask, center=(290, 250), radius=100,
+    mask = cv2.circle(mask, center=face_center, radius=radius,
                       color=(255, 255, 255), thickness=-1)
     temp = np.where(mask==np.array([255, 255, 255]), img, mask)
 
@@ -25,7 +26,7 @@ def create_black_images(img):
         out = cv2.addWeighted(img, i/100, temp, 1-i/100, 0)
 
         out = np.where(mask==np.array([255, 255, 255]), img, out)
-        cv2.imwrite("temp/{}.jpg".format(i), out)
+        cv2.imwrite("{}/{}.jpg".format(outdir, i), out)
         i+=1
 
 def create_blurred_images(img):
@@ -42,16 +43,29 @@ def create_blurred_images(img):
         sigma-=2
 
 
-#for each image in the input directory
+box_dict={}
+with open(box_dict_path) as csv_file:
+    reader = csv.reader(csv_file)
+    for box in list(reader):
+        box_dict[box[0]]=[int(box[1]), int(box[2]), int(box[3]), int(box[4])]
+
+
+# for each image in the input directory
 for file in glob.glob(indir+"/*.jpg"):
 
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     if not os.path.exists('temp'):
         os.makedirs('temp')
+    
+    print(file, box_dict[os.path.basename(file)])
+    box =box_dict[os.path.basename(file)]
+    x,y,w,h = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+    face_center = (int(x+w/2), int(y+h/2))
+    radius = int(w/2 if w>h else h/2) +10
 
     inimg = cv2.imread(file)
-    blurred_imgs = create_black_images(inimg) 
+    blurred_imgs = create_black_images(inimg, face_center, radius) 
 
     filenames= ['temp/{}.jpg'.format(i) for i in range(101)]
     out_gif= '{}/{}-anim.gif'.format(outdir, Path(file).stem)
--- detect_faces.py
+++ detect_faces.py
@@ -6,9 +6,9 @@ Limitations
 
 import glob
 import cv2
-import sys
+import sys, os
 from mtcnn import MTCNN
-import os
+import csv
 
 detector = MTCNN()
 
@@ -21,18 +21,26 @@ ext = ['png', 'jpg']
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
+box_list=[]
 # for file in files:
 for file in glob.glob(indir+"/*.jpg"):
+    print(file)
     # Read the input image
     img = cv2.imread('{}'.format(file))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     face = detector.detect_faces(img)[0]
     x,y,w,h=face['box']
+    box_list.append([os.path.basename(file), x, y, w, h])
+    # box_dict[os.path.basename(file)]=face['box']
 
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     cv2.imwrite(outdir+'/{}'.format(os.path.basename(file)), 
               cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
 
-
+with open('boxes.csv', 'w') as csv_file:  
+    writer = csv.writer(csv_file)
+    writer.writerows(box_list)
+    # for key, value in box_dict.items():
+    #     writer.writerow([key, value])
